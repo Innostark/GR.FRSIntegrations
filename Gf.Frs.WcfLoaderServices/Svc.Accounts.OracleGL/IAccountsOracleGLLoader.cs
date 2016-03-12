fc4aac66f0a01228702e15c5b807904cdf418668
quantumbr <bilal@innostark.com>
2016-03-12 20:08:50 +0000

    WCF Correctly added

---
+++ Gf.Frs.WcfLoaderServices/Svc.Accounts.OracleGL/IAccountsOracleGLLoader.cs
@@ -0,0 +1,15 @@
+﻿using Gf.Frs.LoaderWcfServices.InputOutput.Accounts.OracleGL;
+using System.ServiceModel;
+
+namespace Gf.Frs.WcfLoaderServices.Accounts.OracleGL
+{
+    /// <summary>
+    /// Service contract for the FRS ORACLE GL WCF Loader service
+    /// </summary>
+    [ServiceContract(Name = "FrsOracleGLLoaderService", Namespace = "http://www.gulffinance.com.sa/frs/v1/accounts/oraclegl/operations")]
+    public interface IAccountsOracleGLLoader
+    {
+        [OperationContract]
+        LoadOracleGLResponse LoadOracleGL(LoadOracleGLRequest request);
+    }
+}
---
+++ Gf.Frs.WcfLoaderServices/Svc.Bank.MT940/IBankMT940Loader.cs
@@ -0,0 +1,15 @@
+﻿using Gf.Frs.LoaderWcfServices.InputOutput.Bank.MT940;
+using System.ServiceModel;
+
+namespace Gf.Frs.WcfLoaderServices.Bank.MT940
+{
+    /// <summary>
+    /// Service contract for the FRS MT940 WCF Loader service
+    /// </summary>
+    [ServiceContract(Name = "BankMT940Loader", Namespace = "http://www.gulffinance.com.sa/frs/v1/bank/mt940/operations")]
+    public interface IBankMT940Loader
+    {
+        [OperationContract]
+        LoadMT940Response LoadMT940(LoadMT940Request request);
+    }
+}
